79b1ce89d44347ce4c39ad807c790233763251c6
Lucas Costanza <lucascostanza55@gmail.com>
2021-02-01 23:48:10 -0300

    Segundo commit

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -6,7 +6,7 @@ function mostrar()
 {
 	let nombre;
 	nombre=prompt("Ingrese su nombre");
-	document.getElementById("txtIdNombre").value=nombre;
-	
+	document.getElementById("txtIdNombre").value=nombre;	
 }
 
+//Terminado
